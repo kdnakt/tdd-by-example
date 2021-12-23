86697b68d164d5757961cb0a239e2b9772293ebf
kdnakt <a.kid.1985@gmail.com>
2021-12-23 10:21:01 +0900

    fake implement of bank.rate()

--- app/src/main/java/money/Sum.java
+++ app/src/main/java/money/Sum.java
@@ -8,7 +8,7 @@ class Sum implements Expression {
         this.addend = addend;
     }
     @Override
-    public Money reduce(String to) {
+    public Money reduce(Bank bank, String to) {
         int amount = augend.amount + addend.amount;
         return new Money(amount, to);
     }
