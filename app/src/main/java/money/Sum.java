088f628a9a8211dbd3cdfd6fafac6f74a3b14a36
kdnakt <a.kid.1985@gmail.com>
2021-12-26 12:06:31 +0900

    change Money to Expression

--- app/src/main/java/money/Sum.java
+++ app/src/main/java/money/Sum.java
@@ -1,11 +1,15 @@
 package money;
 
 class Sum implements Expression {
-    Money augend;
-    Money addend;
-    Sum(Money augend, Money addend) {
+    Expression augend;
+    Expression addend;
+    Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
+    }
+    @Override
+    public Expression plus(Expression addend) {
+        return null;
     }
     @Override
     public Money reduce(Bank bank, String to) {
